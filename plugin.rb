e3b85552b22a2a4173ab4207eecf9dc27b2ea603
Thorben Egberts <tegberts@gmail.com>
2018-05-13 17:29:55 +0200

    ADD user custom user fields

--- plugin.rb
+++ plugin.rb
@@ -4,4 +4,13 @@
 # authors: plentymarkets GmbH
 # url: https://github.com/plentymarkets/switch-content-language
 
+#require 'cgi'
+
 enabled_site_setting :enable_content_language_switching
+
+after_initialize do
+
+  User.register_custom_field_type('content_language_en', :boolean)
+  User.register_custom_field_type('content_language_de', :boolean)
+
+end
